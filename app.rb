5a3f68500a25b37f2675b1813d8c4a2342a5543c
Stella Redler <stella.redler@elev.ga.ntig.se>
2025-01-28 09:36:28 +0100

    Update 1

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+
+get ('/') do
+    slim :home
+end
+
+get ('/forum') do
+    slim :forum
+end
+
+get ('/profil') do
+    slim :profile
+    db = SQLite3::Database.new("db/horoskop.db")
+    result = db.execute("SELECT * FROM users")
+    slim(:users, locals:{users: result})
+end
