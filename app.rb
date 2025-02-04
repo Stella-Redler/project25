444ac8680e44f9d836441269878a34df8c7f37a8
Stella Redler <stella.redler@elev.ga.ntig.se>
2025-02-04 10:12:36 +0100

    update 4

--- app.rb
+++ app.rb
@@ -14,10 +14,28 @@ get('/forum') do
     slim :forum
 end
 
-get('/users/profile') do
+get('/profile') do
     db = SQLite3::Database.new("db/horoskop.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM users")
-    p result
-    slim(:users, locals:{users: result})
+    slim(:profile, locals:{users:result})
+end
+
+get('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+
+    if password == password_confirm
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new("db/horoskop.db")
+        db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",{username,password_digest})
+        redirect('/')
+    else
+        "Lösenorden matchar inte"
+    end
+end
+
+get('/showlogin') do
+    slim(:login)
 end
